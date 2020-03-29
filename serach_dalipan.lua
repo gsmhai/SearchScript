108c0b50e066ff31374c3f075e7d4d5722afe43a
gsmhai <gsmhai@qq.com>
2020-03-29 16:46:36 +0800

    0.0.2 update for dalipan

--- serach_dalipan.lua
+++ serach_dalipan.lua
@@ -1,9 +1,11 @@
 local curl = require "lcurl.safe"
+local cookie = ""
+local referer_url = ""
 
 script_info = {
 	["title"] = "大力盘",
 	["description"] = "https://www.dalipan.com/",
-	["version"] = "0.0.1",
+	["version"] = "0.0.2",
 }
 
 function request(url,header)
@@ -37,6 +39,12 @@ function onSearch(key, page)
 	local data = request("https://www.dalipan.com/search?keyword=" .. pd.urlEncode(key) .. "&page=" .. page, header)
 	local result = {}
 	local start = 1
+	local ctime= os.time()	
+	local _,_,ck = string.find(data, "https://hm.baidu.com/hm.js??(.-)\"")
+	if ck then
+		cookie = "Hm_lvt_" .. ck .. "=".. ctime .. "; Hm_lpvt_" .. ck  .. "="..(ctime+15)
+	end
+	referer_url = "https://www.dalipan.com/search?keyword=" .. pd.urlEncode(key) .. "&page=" .. page
 	
 	while true do
 	
@@ -58,26 +66,34 @@ function onSearch(key, page)
 end
 
 
-function parseDetail(id)
+function parseDetail(item)
 
-		local deatil_url = "https://www.dalipan.com/detail/".. id
 		header = {
 			"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
-			"referer: " .. deatil_url,
+			"referer: " .. referer_url,
+			"cookie: " .. cookie,
 		}
-		local image_url = "https://www.dalipan.com/images/recommand.png"
-		request(image_url, header)
-		local api_url = 'https://www.dalipan.com/api/private?id=' .. id
-		local ret = request(api_url, header)
-		local a, c, pwd, url = string.find(ret, '"pwd": "(.-)",.-"url": "(.-)"')
-		
-		return url .. " " .. pwd
-
+		--local image_url = "https://www.dalipan.com/images/recommand.png"
+		local deatil_url = "https://www.dalipan.com/detail/".. item.id
+		local ret = request(deatil_url, header)
+		local _,_,url = string.find(ret, "<a target=\"_blank\" href=\"(.-)\"")
+		local _,_,pwd = string.find(ret,">提取密码</span>(.-)<span")
+		if pwd == nil then
+			return url
+		else
+			pwd = string.gsub(pwd, "\r","")
+			pwd = string.gsub(pwd, "\n","")
+			pwd = string.gsub(pwd," ", "")
+			return url .. " " .. pwd
+		end
 end
 
 function onItemClick(item)
 
-	local url = parseDetail(item.id)
+	local url = parseDetail(item)
+	if url == nil then
+		return ACT_MESSAGE, '获取URL失败'
+	end
 	return ACT_SHARELINK, url 
 end
 
